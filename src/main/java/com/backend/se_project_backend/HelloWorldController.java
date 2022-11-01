cdb5a2d29fe602d1d40863f7d820e63a1aed28e0
andreea păun minunata <andreeapaun321@gmail.com>
2022-11-01 09:47:56 +0200

    added index.html

--- src/main/java/com/backend/se_project_backend/HelloWorldController.java
+++ src/main/java/com/backend/se_project_backend/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.backend.se_project_backend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class HelloWorldController {
-    @GetMapping("/hello-world")
-    public String HelloWorld(){
-        return "Bori, Andreea and Radu say hi!";
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
     }
+
+    @GetMapping("/hello-world")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "from Andreea, Bori and Radu") String name) {
+        return String.format("Hello %s!", name);
+    }
+
 }
